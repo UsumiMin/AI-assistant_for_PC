f821257ce9dce5fb6c0c09ec7dfa441ea9631b20
Katerina Kriveleva <KaterinaKrivelyova@yandex.ru>
2026-06-02 23:26:48 +0500

    Add pages for buttons

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let win;
 
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1280,
     height: 720,
     frame: false,
@@ -14,10 +17,46 @@ function createWindow () {
       webSecurity: false,
     }
   });
-
+  
   win.loadFile('index.html');
 
     win.webContents.openDevTools();
 }
 
+ipcMain.on('open-window', (event, filePage) => {
+  if (win) {
+    win.setAlwaysOnTop(false);
+  }
+
+  let childWindow = new BrowserWindow({
+    width: 600,
+    height: 500,
+    parent: win,
+    modal: true,
+    resizable: false,
+    frame: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  childWindow.loadFile(filePage);
+  
+  childWindow.setMenu(null);
+  childWindow.center();
+  childWindow.on('closed', () => {
+    if (win) {
+      win.setAlwaysOnTop(true);
+      win.focus();
+    }
+  });
+});
+
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
